c4369e3953c9f2a69cf4ca1d83623a5d31ca8b26
Tsyren Ochirov <nsu1team@gmail.com>
2015-12-22 15:55:30 +0100

    Provide options for orders fetch method

--- lib/beslist/client.rb
+++ lib/beslist/client.rb
@@ -10,17 +10,19 @@ module Beslist
                                                     :shop_id   => options[:shop_id])
       end
 
-      def orders(date_from, date_to)
+      def orders(options)
+        date_from, date_to = options.delete(:date_from), options.delete(:date_to)
         response = @connection.request do
           @connection.interface.get do |req|
             req.url Beslist::API::Config.prefix + '/shoppingcart/shop_orders/'
             req.params = {
               checksum: @connection.checksum(date_from, date_to),
               client_id: @connection.options[:client_id],
-              shop_id: @connection.options[:shop_id],
-              date_from: date_from,
-              date_to: date_to
+              shop_id: @connection.options[:shop_id]
             }
+
+            req.params.merge!(date_from: date_from) if date_from
+            req.params.merge!(date_to: date_to) if date_to
             req.params.merge!(output_type: 'test', test_orders: '1') if Beslist::API::Config.mode == 'sandbox'
           end
         end
