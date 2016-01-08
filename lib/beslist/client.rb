0985f722bff1144b22625fb68bdbd02a2e55bc93
Tsyren Ochirov <nsu1team@gmail.com>
2016-01-08 15:53:55 +0100

    Provide ability to pass test products for sandbox env

--- lib/beslist/client.rb
+++ lib/beslist/client.rb
@@ -24,7 +24,10 @@ module Beslist
 
             req.params.merge!(date_from: date_from) if date_from
             req.params.merge!(date_to: date_to) if date_to
-            req.params.merge!(output_type: 'test', test_orders: '250') if Beslist::API::Config.mode == 'sandbox'
+            if Beslist::API::Config.mode == 'sandbox'
+              req.params.merge!(output_type: 'test', test_orders: '250')
+              req.params.merge!(test_products: options[:test_products]) unless options[:test_products].nil?
+            end
           end
         end
 
